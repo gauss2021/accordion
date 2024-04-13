f2146102aa60a65dfac0c494e2fb9ba3a2d2d8df
gauss2021 <bioussalavandavydson@gmail.com>
2024-04-13 10:38:01 +0100

    Retour a la mise en place du singleAccordion

--- src/composants/accordion.jsx
+++ src/composants/accordion.jsx
@@ -4,40 +4,36 @@ import './../styles/style.css';
 
 export default function Accordion(){
 
-    const [selected,setSelected]=useState([]);
+    const [selected,setSelected]=useState(null);
 
-    const [typeOfAccordion,setTypeOfAccordion]=useState(0);
+    const [enableMultipleSelection,setEnableMultipleSelection]=useState(false);
+
+    const [storeId,setStoreId]=useState([]);
 
 
-    function handleActifAccordion(id){
+    function SingleAccordion(id){
 
-        console.log('On clique');
-        if(selected.includes(id)){
-          const filterArray= selected.filter((identifiant)=>identifiant!=id);
-          console.log('filter Array',filterArray);
-          setSelected(...filterArray);
-        }else{
-            selected.push(id);
-        }
+        setSelected(id ===selected ? null:id);
         
         console.log(selected)
 
         
     }
 
-    function changeTypeOfAccordion(type){
-        setTypeOfAccordion(type);
+    function MultipleAccordion(id){
+
+        
+
     }
 
+  
     return (<div className="app">
         <h5>Ceci est un accordion</h5>
 
         <p>Le but de ce mini projet est d'appliquer mes connaissance en react js, donc ce projet consite a creer un accordeon qui par defaut ne permet d'ouvrir qu'un seul accordeon, neanmoins on peut ouvrir jusqu'a deux accordeons en cliquant sur le boutton multiple accordeons </p>
 
         <div className="bouttons">
-            <button onClick={()=>{changeTypeOfAccordion(0)}}>Mono accordion</button>
-
-            <button onClick={()=>{changeTypeOfAccordion(1)}}>Multiple accordeons</button>
+            <button onClick={()=>{setEnableMultipleSelection(!enableMultipleSelection)}}>Enable Multiple Selection</button>
         </div>
        
        <hr style={{marginTop:"8px"}} />
@@ -47,13 +43,13 @@ export default function Accordion(){
            {data.length > 0 ? 
 
             data.map(item=> <div key={"item"+item.id} className="accordion">
-                <div onClick={()=>handleActifAccordion(item.id)} className="accordion--header">
+                <div onClick={()=>SingleAccordion(item.id)} className="accordion--header">
                     <span>+</span>
                     <h3>{item.title}</h3>
                    
                 </div>
 
-                {selected.includes(item.id) ? (<div className="accordion--body">
+                {selected ===item.id ? (<div className="accordion--body">
                     <p>{item.description}</p>
                 </div>):null}
 
